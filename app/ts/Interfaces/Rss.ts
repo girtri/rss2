1d3e42253b79fb469f8caad4b3c0081bc0840549
gerry <girtri@libero.it>
2017-10-30 17:19:21 +0100

    inizio implemnentazione

---
+++ app/ts/Interfaces/Rss.ts
@@ -0,0 +1,14 @@
+
+export interface IRssItem 
+{
+	description: string;
+	link: string;
+	pubdate: string;
+	title: string;
+}
+
+export interface IFeed 
+{
+	title: string;
+	items: IRssItem[];
+}
---
+++ app/ts/Services/IFeedMe.ts
@@ -0,0 +1,9 @@
+
+import { IRssItem } from "../Interfaces/Rss";
+
+export interface IFeedMe 
+{
+	new ( flag?: boolean ): NodeJS.WritableStream;
+	on( event: "title", onTitle: ( title: string ) => void): void;
+	on( event: "item", onItem: ( item: IRssItem ) => void ): void;
+}
